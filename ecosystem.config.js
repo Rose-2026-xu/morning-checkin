99eb2fabbd62332da4246b80a48dbb36c4b42b8f
Rose-2026-xu <dongling_xuxu@163.com>
2026-09-12 08:34:08 +0800

    update ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -4,7 +4,7 @@ module.exports = {
     script: 'server/server.js',
     cwd: '/opt/morning-checkin',
     env: {
-      PORT: 3001,
+      PORT: 3002,
       JWT_SECRET: 'mc-jwt-prod-2026-s3cret'
     }
   }]
